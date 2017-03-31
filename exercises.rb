53777df1e63bcd958bb2a60003ca7029cbdbe370
Alex <alex.mahinko@gmail.com>
2017-03-31 19:37:05 -0400

    excercise 0 done

---
+++ exercises.rb
@@ -0,0 +1,17 @@
+# Exercise 0
+
+array = ["black", "red", 25, 30, "19? I dunno.", "heads", "heads", "heads", "heads", "heads", "Maynard Keenan", "Carl Hungus", "A third guy", :black, :red]
+
+hash = {
+
+  autophagia: "The compulsion to eat oneself.",
+  schadenfreude: "Pleasure derived from the misfortune of others.",
+  energy: "The potential to do work.",
+  fight_club: "1999",
+  blade_runner: "1982",
+  eastern_promises: "2007",
+  tijuana: "1.3 million",
+  sydney: "4.2 million",
+  hong_kong: "7.2 million"
+
+}
