38d7d4cb100959324343b8dc80496b16c771d49a
Alex <alex.mahinko@gmail.com>
2017-03-31 19:41:49 -0400

    excercise 0 done

--- exercises.rb
+++ exercises.rb
@@ -1,17 +1,30 @@
 # Exercise 0
 
-array = ["black", "red", 25, 30, "19? I dunno.", "heads", "heads", "heads", "heads", "heads", "Maynard Keenan", "Carl Hungus", "A third guy", :black, :red]
+colors = ["black", "red"]
 
-hash = {
+ages = [25, 30, 19]
+
+coinflips = ["heads", "heads", "heads", "heads", "heads"]
+
+people = ["Maynard Keenan", "Carl Hungus", "A third guy"]
+
+sym_colors = [:black, :red]
+
+words = {
 
   autophagia: "The compulsion to eat oneself.",
   schadenfreude: "Pleasure derived from the misfortune of others.",
-  energy: "The potential to do work.",
+  energy: "The potential to do work."
+}
+
+movies = {
   fight_club: "1999",
   blade_runner: "1982",
-  eastern_promises: "2007",
+  eastern_promises: "2007"
+}
+
+cities = {
   tijuana: "1.3 million",
   sydney: "4.2 million",
   hong_kong: "7.2 million"
-
 }
